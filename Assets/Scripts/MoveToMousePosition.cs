5ebfa5fb7d872b079d0fe6b5133876dc2d9b4118
Wilkinson Souza <wilki.darolt@gmail.com>
2018-02-18 19:36:32 -0300

    Add script to player, allowing it to move towards the last mouse click position.

---
+++ Assets/Scripts/MoveToMousePosition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToMousePosition : MonoBehaviour {
+    public float speed = 1.5f;
+    private Vector3 target;
+
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
+
+
