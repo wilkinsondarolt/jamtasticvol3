1434e3cc6dca6371d5eb533b131f70a27b77d41c
Wilkinson Souza <wilki.darolt@gmail.com>
2018-02-21 23:12:06 -0300

    Change the dialog kind to a floating baloon.

--- Assets/Scripts/FloatingDialog.cs
+++ Assets/Scripts/FloatingDialog.cs
@@ -1,16 +1,49 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class FloatingDialog : MonoBehaviour {
-    public Text nameLabel;
+    private Text textDialog;
+    public Image baloonDialog;
+    
     public float xDisp;
     public float yDisp;
+    public string[] textToDisplay;
+
+    private void Start()
+    {
+        textDialog = baloonDialog.GetComponentInChildren<Text>();
+        StopAllCoroutines();
+        StartCoroutine(DisplayDialog());
+    }
+
+    IEnumerator DisplayDialog()
+    {      
+        foreach (string Sentence in this.textToDisplay)
+        {
+            textDialog.text = "";
+            foreach (char Letter in Sentence)
+            {
+                textDialog.text += Letter;
+                yield return null;
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        DisableDialogBaloon();
+    }
 
     void Update ()
     {
         Vector3 wantedPos = Camera.main.WorldToViewportPoint(this.gameObject.transform.position);
-        nameLabel.transform.position = new Vector3(wantedPos.x * Screen.width + xDisp, 
-                                                   wantedPos.y * Screen.height + yDisp, 
-                                                   40);
+        baloonDialog.transform.position = new Vector3(wantedPos.x * Screen.width + xDisp, 
+                                                      wantedPos.y * Screen.height + yDisp, 
+                                                      40);
+    }
+
+    void DisableDialogBaloon()
+    {
+        baloonDialog.gameObject.SetActive(false);
+        
     }
 }
