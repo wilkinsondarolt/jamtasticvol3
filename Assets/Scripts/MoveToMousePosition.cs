54a5568266b8fd40e044ac5736ced9322273f2e9
Wilkinson Souza <wilki.darolt@gmail.com>
2018-02-22 23:26:12 -0300

    Adds Player animation and background image.

--- Assets/Scripts/MoveToMousePosition.cs
+++ Assets/Scripts/MoveToMousePosition.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 
 public class MoveToMousePosition : MonoBehaviour {
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     public float speed = 1.5f;
     private Vector3 target;
 
     void Start()
     {
         target = transform.position;
+        animator = this.GetComponent<Animator>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -19,6 +23,10 @@ public class MoveToMousePosition : MonoBehaviour {
             target.y = this.transform.position.y;
             target.z = transform.position.z;
         }
+        if (animator)   
+            animator.SetBool("Walking", (transform.position != target));
+        if (spriteRenderer)
+            spriteRenderer.flipX = (target - transform.position).x < 0;            
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
     }
 }
