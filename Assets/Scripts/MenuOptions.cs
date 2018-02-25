5060b3907552981abd6e652fa52f4f67cad66b0c
Wilkinson Souza <wilki.darolt@gmail.com>
2018-02-25 17:45:58 -0300

    Adds more scenes (Menu and ending).

---
+++ Assets/Scripts/MenuOptions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions : MonoBehaviour {
+
+    public void StartGame(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
