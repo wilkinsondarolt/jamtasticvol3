3375cb347fe32d9733d860b47296dffd799005ec
Wilkinson Souza <wilki.darolt@gmail.com>
2018-02-24 23:24:07 -0300

    Adds a script to the main camera, so it can follow the player, which is a nice thing to have

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+    public float MinX;
+    public float MaxX;
+
+    private void Update()
+    {
+        Camera.main.transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinX, MaxX),
+                                                     Camera.main.transform.position.y,
+                                                     Camera.main.transform.position.z);
+    }
+}
